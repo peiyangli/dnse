b4982838d7bc2b3f22c1def00be73c54f327f9f5
pei <pei@anvei.com>
2024-10-24 09:54:51 +0800

    answer A

--- cmd/dnse/main.go
+++ cmd/dnse/main.go
@@ -1,13 +1,30 @@
 package main
 
 import (
+	"flag"
 	"log"
+	"net"
 
 	"github.com/miekg/dns"
 )
 
+type Flags struct {
+	Addr string
+}
+
+var gFlags Flags
+
+func Init() {
+	flag.StringVar(&gFlags.Addr, "addr", ":53", "ip:port")
+	flag.Parse()
+}
+
 func main() {
 	log.SetFlags(11)
+	Init()
+
+	log.Println(gFlags)
+
 	dns.NewServeMux()
 	dns.HandleFunc(".", func(w dns.ResponseWriter, req *dns.Msg) {
 		log.Println(req)
@@ -16,13 +33,16 @@ func main() {
 
 		log.Println(req.Question[0].Name)
 
-		m.Answer = append(m.Answer, &dns.A{})
+		m.Answer = append(m.Answer, &dns.A{
+			Hdr: dns.RR_Header{Name: req.Question[0].Name, Rrtype: dns.TypeA, Class: dns.ClassINET, Ttl: 0},
+			A:   net.ParseIP("127.0.0.1"),
+		})
 
-		m.Extra = make([]dns.RR, 1)
-		m.Extra[0] = &dns.TXT{Hdr: dns.RR_Header{Name: m.Question[0].Name, Rrtype: dns.TypeTXT, Class: dns.ClassINET, Ttl: 0}, Txt: []string{"Hello world"}}
+		m.Extra = append(m.Extra, &dns.TXT{Hdr: dns.RR_Header{Name: m.Question[0].Name, Rrtype: dns.TypeTXT, Class: dns.ClassINET, Ttl: 0}, Txt: []string{"Hello world"}})
 		w.WriteMsg(m)
 	})
-	err := dns.ListenAndServe(":2053", "udp", dns.DefaultServeMux)
+
+	err := dns.ListenAndServe(gFlags.Addr, "udp", dns.DefaultServeMux)
 
 	log.Println("Stop", err)
 }
