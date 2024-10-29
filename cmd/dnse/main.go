c2df0e5fb35d967410ee87485b04cd659c2c945f
pei <pei@anvei.com>
2024-10-29 17:57:54 +0800

    txt

--- cmd/dnse/main.go
+++ cmd/dnse/main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"log"
 	"net"
+	"time"
 
 	"github.com/miekg/dns"
 )
@@ -32,17 +33,29 @@ func main() {
 		m.SetReply(req)
 
 		log.Println(req.Question[0].Name)
-
-		m.Answer = append(m.Answer, &dns.A{
-			Hdr: dns.RR_Header{Name: req.Question[0].Name, Rrtype: dns.TypeA, Class: dns.ClassINET, Ttl: 0},
-			A:   net.ParseIP("127.0.0.1"),
-		})
-
-		m.Answer = append(m.Answer, &dns.TXT{
-			Hdr: dns.RR_Header{Name: req.Question[0].Name, Rrtype: dns.TypeA, Class: dns.ClassINET, Ttl: 0},
-			Txt: []string{"world"},
-		})
-
+		if req.IsTsig() != nil {
+			m.SetTsig(req.Extra[len(req.Extra)-1].(*dns.TSIG).Hdr.Name, dns.HmacMD5, 300, time.Now().Unix())
+		}
+		qt := req.Question[0].Qtype
+		switch qt {
+		default:
+			fallthrough
+		case dns.TypeTXT:
+			m.Answer = append(m.Answer, &dns.TXT{
+				Hdr: dns.RR_Header{Name: req.Question[0].Name, Rrtype: dns.TypeTXT, Class: dns.ClassINET, Ttl: 0},
+				Txt: []string{"world"},
+			})
+		case dns.TypeAAAA, dns.TypeA:
+			m.Answer = append(m.Answer, &dns.A{
+				Hdr: dns.RR_Header{Name: req.Question[0].Name, Rrtype: dns.TypeA, Class: dns.ClassINET, Ttl: 0},
+				A:   net.ParseIP("127.0.0.1"),
+			})
+			m.Answer = append(m.Answer, &dns.TXT{
+				Hdr: dns.RR_Header{Name: req.Question[0].Name, Rrtype: dns.TypeTXT, Class: dns.ClassINET, Ttl: 0},
+				Txt: []string{"world"},
+			})
+		case dns.TypeAXFR, dns.TypeIXFR:
+		}
 		m.Extra = append(m.Extra, &dns.TXT{Hdr: dns.RR_Header{Name: m.Question[0].Name, Rrtype: dns.TypeTXT, Class: dns.ClassINET, Ttl: 0}, Txt: []string{"Hello world"}})
 		w.WriteMsg(m)
 	})
