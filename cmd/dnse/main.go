354ecb806b63a356563ceb000b880a30dd73c47f
pei <pei@anvei.com>
2024-10-29 17:51:50 +0800

    txt record

--- cmd/dnse/main.go
+++ cmd/dnse/main.go
@@ -38,6 +38,11 @@ func main() {
 			A:   net.ParseIP("127.0.0.1"),
 		})
 
+		m.Answer = append(m.Answer, &dns.TXT{
+			Hdr: dns.RR_Header{Name: req.Question[0].Name, Rrtype: dns.TypeA, Class: dns.ClassINET, Ttl: 0},
+			Txt: []string{"world"},
+		})
+
 		m.Extra = append(m.Extra, &dns.TXT{Hdr: dns.RR_Header{Name: m.Question[0].Name, Rrtype: dns.TypeTXT, Class: dns.ClassINET, Ttl: 0}, Txt: []string{"Hello world"}})
 		w.WriteMsg(m)
 	})
